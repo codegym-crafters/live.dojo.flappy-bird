0bcb5601057c19c20720ae13cb29f8997c4f1f6f
son <son@codegym.vn>
2021-05-23 11:08:39 +0700

    extract method

--- scripts/game.js
+++ scripts/game.js
@@ -19,20 +19,32 @@ function Game () {
   this.getPipes = function () {
     return _pipes
   }
-
   this.tick = function () {
     removeOutOfScreenPipes()
-    _pipes.forEach(pipe => pipe.driftingBack())
-    _bird.fall()
+    letAllPipeDriftingBack()
+    letBirdFallingDown()
+    initAnotherPipeIfNeeded()
+    viewEngine.refresh()
+  }
+
+  function initAnotherPipeIfNeeded () {
     if (!_timeToNextPipe--) {
       _pipes.push(new BottomPipe())
       _timeToNextPipe = Math.round(Math.random() * 25) + 75
     }
-    viewEngine.refresh()
+  }
+
+  function letBirdFallingDown () {
+    _bird.fall()
+  }
+
+  function letAllPipeDriftingBack () {
+    _pipes.forEach(pipe => pipe.driftingBack())
   }
 
   function removeOutOfScreenPipes () {
-    if (_pipes.length && _pipes[0].isOutOfScreen()) {
+    if (!_pipes.length) return
+    if (_pipes[0].isOutOfScreen()) {
       _pipes = _pipes.slice(1)
     }
   }
