7d354c40ef95c477852d50946bff471eecf66b4f
son <son@codegym.vn>
2021-06-20 12:37:07 +0700

    serve with webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 'use strict'
 
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
   entry: './src/js/main.js',
   output: {
     path: __dirname + '/build/dist',
     filename: '[name].bundle.js'
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
